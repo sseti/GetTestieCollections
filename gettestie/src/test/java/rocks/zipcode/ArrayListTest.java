9e55afe4484c92e36083de7b83b2a10b9b1aa227
Sandy Setiawan <ssetiawan@zipcodes-MacBook-Pro-3.local>
2020-03-05 17:44:00 -0500

    passed hashmap test

--- gettestie/src/test/java/rocks/zipcode/ArrayListTest.java
+++ gettestie/src/test/java/rocks/zipcode/ArrayListTest.java
@@ -1,4 +1,59 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ArrayListTest {
+
+    private ArrayList<String> array;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        array = new ArrayList<>();
+        array.add("Bird");
+        array.add("Elephant");
+        array.add("Rhinoceros");
+        array.add("pig");
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception { }
+
+    @org.junit.Test
+    public void TestArrayListGetIndex() { Assert.assertEquals("Bird", array.get(0)); }
+
+    @Test
+    public void TestArrayListContains1() { Assert.assertEquals(true, array.contains("Bird")); }
+
+    @Test
+    public void TestArrayListContains2() { Assert.assertEquals(false, array.contains("Ants")); }
+
+    @Test
+    public void TestArrayListSubList() {
+        List<String> arrayList2 = array.subList(1, 3);
+        Assert.assertEquals(array.get(2), arrayList2.get(1));
+    }
+
+    @Test
+    public void TestArrayListRemove() { Assert.assertEquals(true, array.remove("Elephant")); }
+
+    @Test
+    public void TestArrayListAddAll() {
+        ArrayList<String> arrayList2 = new ArrayList<String>();
+        arrayList2.add("Duck");
+        arrayList2.add("Dog");
+        arrayList2.add("Cat");
+        Assert.assertEquals(true, array.addAll(arrayList2));
+    }
+
+    @Test
+    public void TestArrayListReverse() {
+        Collections.reverse(array);
+        Assert.assertEquals(array.subList(0,4), array);
+    }
 }
+
--- gettestie/src/test/java/rocks/zipcode/HashMapTest.java
+++ gettestie/src/test/java/rocks/zipcode/HashMapTest.java
@@ -1,4 +1,49 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 public class HashMapTest {
+
+    HashMap<String, Integer> Map;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        Map = new HashMap<>();
+        Map.put("A", 1);
+        Map.put("B", 2);
+        Map.put("C", 3);
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void TestHashMapContainsKey() {
+        Assert.assertTrue(Map.containsKey("B"));
+    }
+
+    @Test
+    public void TestHashMapEmpty() {
+        Assert.assertFalse(Map.isEmpty());
+    }
+
+    @Test
+    public void TestHashMapRemove() {
+        Assert.assertTrue(Map.remove("A", 1));
+    }
+
+    @Test
+    public void TestHashMapReplace() {
+        Assert.assertEquals((Integer) 3, Map.replace("C", 4));
+    }
+
+    @Test
+    public void TestHashMapClear() { Map.clear();
+        Assert.assertEquals(true, Map.isEmpty());
+    }
 }
+
